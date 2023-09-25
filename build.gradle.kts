bbf887673fed82709902277aa4e0ff65bccf94a7
shinseongsu <tlstjdtn321@naver.com>
2023-09-25 21:21:39 +0900

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm")
+    kotlin("kapt")
+    kotlin("plugin.spring") apply false
+
+    id("org.springframework.boot") apply false
+    id("io.spring.dependency-management")
+}
+
+java.sourceCompatibility = JavaVersion.valueOf("VERSION_${property("javaVersion")}")
+
+allprojects {
+    group = "${property("projectGroup")}"
+    version = "${property("applicationVersion")}"
+
+    repositories {
+        mavenCentral()
+    }
+}
+
+subprojects {
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "org.jetbrains.kotlin.kapt")
+    apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "io.spring.dependency-management")
+
+    dependencies {
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+        annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+        kapt("org.springframework.boot:spring-boot-configuration-processor")
+    }
+
+    java.sourceCompatibility = JavaVersion.valueOf("VERSION_${property("javaVersion")}")
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "${project.property("javaVersion")}"
+        }
+    }
+}
